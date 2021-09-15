67a60a334c7486139a1ac7ad9cd82bca96665e28
FabienDragne <fabiendragne@gmail.com>
2021-09-15 19:17:16 +0200

    premier commit

---
+++ cheat_sheet.py
@@ -0,0 +1,105 @@
+# Afficher dans la console
+# print()
+
+####################################################
+                        # print
+####################################################
+
+print("Bienvenue dans ce fichier cheat sheet dédié à Python!")
+print("ver. 1")
+print()
+
+# je peux print des str et des int, 
+# mais dans le cas d'un int, il devra être seul ou cast préalablement en str
+# on ne peut concaténer un int avec un str autrement
+
+####################################################
+                        # input()
+####################################################
+
+# input("Veuillez entrer une valeur : ")
+
+# arrête le déroulement du programme le temps que la réponse soit entrée par l'utilisateur
+
+# test_input = input("Veuillez entrer une valeur qui sera sauvegardé dans la variable test_input ")
+# print("La valeur entrée est : " + test_input)
+
+# input renvoit toujours un string
+
+
+####################################################
+                    # commentaire
+####################################################
+
+# commentaire sur une seule ligne
+
+""" Commentaire
+    sur
+    plusieurs
+    lignes """
+
+####################################################
+                    # type()
+####################################################
+
+# afficher le type d'une variable
+
+# test_type_string = "texte d'exemple"
+# test_type_int = 17
+
+# print(type(test_type_int))
+# print(type(test_type_string))
+
+####################################################
+                # Conversion (cast)
+####################################################
+
+# convertir un string vers un int
+
+# variable_string_test = "33"
+# calcul_test = int(variable_string_test) + 14
+
+# convertir un integer en string
+
+# variable_int_test = 147
+# print(str(variable_int_test))
+
+####################################################
+                # try / except
+####################################################
+
+# Pour prévoir le comportement du programme en cas d'erreur
+
+# exemple :
+# je déclare un string que je vais tenter d'utiliser dans une opération arithmétique
+
+# print("Entrez une valeur de test")
+# valeur_de_test = input()
+
+# try:
+#     nouvelle_valeur = int(valeur_de_test) + 1               # tentative
+# except:
+#     print("L'opération ne peut fonctionner")                # action en cas d'échec
+#     print("vous devez rentrer un nombre.")
+
+####################################################
+                # boucle while
+####################################################
+
+# boucle while qui va içi s'effectuer 5 fois
+
+# n = 0
+
+# while n < 5:
+#     print("ligne " + str(n))
+#     n += 1
+
+# On peut utiliser "not" dans les conditions d'une boucle while
+
+password = ""
+
+while not password == "1234":
+    password = input("What is the password? ")
+
+print("password is correct, you have access to the account.")
+
---
+++ test_cours.py
@@ -0,0 +1,5 @@
+print("What's your name?")
+name = input()
+print("How old are your?")
+age = input()
+print("Your name is " + name + " and you are " + age + " years old.")
